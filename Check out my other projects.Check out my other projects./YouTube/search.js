5cf6cd3cefd08f30859852c4fdfe59846cc4ae89
GP_4465 <olusyamazurenco@gmail.com>
2025-09-21 17:37:31 +0300

    Create search.js

---
+++ Check out my other projects.Check out my other projects./YouTube/search.js
@@ -0,0 +1,5 @@
+// Поиск по видео (гигантский скрипт для примера)
+console.log("Search JS loaded");
+document.getElementById('searchBox').addEventListener('input', function(){
+    console.log('Searching:', this.value);
+});
