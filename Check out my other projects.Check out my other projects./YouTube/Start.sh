9f419422c06e0c7a05cf58ae077d8ee486019b4e
GP_4465 <olusyamazurenco@gmail.com>
2025-09-21 17:31:55 +0300

    Update Start.sh

--- Check out my other projects.Check out my other projects./YouTube/Start.sh
+++ Check out my other projects.Check out my other projects./YouTube/Start.sh
@@ -1,1 +1,10 @@
-
+#!/bin/sh
+echo "🚀 Launching MyTube Clone..."
+echo "Checking Python3..."
+if ! command -v python3 &> /dev/null
+then
+    echo "Python3 not found. Please install Python3 first."
+    exit
+fi
+echo "Starting server..."
+python3 server.py
