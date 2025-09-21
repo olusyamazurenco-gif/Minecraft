f190cd2e5d94eb6f9f0701f1805696b494472e23
GP_4465 <olusyamazurenco@gmail.com>
2025-09-21 16:56:27 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from utils import greet_player
+
+def main():
+    print("Welcome to the Minecraft Project!")
+    player_name = input("Enter your player name: ")
+    greet_player(player_name)
+
+if __name__ == "__main__":
+    main()
+  
